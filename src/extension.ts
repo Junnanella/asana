4382bc437265f9451da92cbc50dc99cd7315b2d9
Junella Caringal <junellacaringal@gmail.com>
2024-03-22 14:30:38 -0700

    detect asana link properly on a line

--- src/extension.ts
+++ src/extension.ts
@@ -18,23 +18,45 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.window.showInformationMessage("Hello World from Asana!");
   });
 
-  const isAsanaLink = (text: string): boolean => {
-    // If not an actual Asana link, then return false
-    return text.toLocaleLowerCase().includes("asana");
+  const regex = /https?:\/\/app\.asana\.com\/0\/[0-9]+\/([0-9]+)(\/f)?/i;
+
+  const getAsanaTaskId = (text: string): string => {
+    // Format 1: https://app.asana.com/0/1150527741687836123213/1206911076351813
+    // Format 2: https://app.asana.com/0/1150527741687836/1206911076351813/f
+
+    const match = text.match(regex);
+
+    if (!match) {
+      // This should never happen because this is only called if
+      // the hover provider detects a regex match
+      throw new Error("Unexpected invalid Asana URL");
+    }
+
+    return match[1];
   };
 
   const hoverProvider: vscode.HoverProvider = {
+    // TODO: Support multiple URLs in one line later
     provideHover(document, position, token) {
-      // TODO:
-      // https://asana.com/123/456 <= If we see this, show a tooltip with the task name (make an API call)
-      const range = document.getWordRangeAtPosition(position);
-      const word = document.getText(range);
+      const line = document.lineAt(position.line);
+      const match = line.text.match(regex);
 
-      if (isAsanaLink(word)) {
-        return new vscode.Hover(
-          `Hello from Asana! You are hovering over ${word}`
-        );
+      // Link doesn't even appear in the line
+      if (!match) {
+        return;
       }
+
+      const asanaLinkIndex = line.text.indexOf(match[0]);
+      const lengthOfAsanaLink = match[0].length;
+
+      // Link does appear, but I'm not hovering over it
+      if (position.character < asanaLinkIndex || position.character > asanaLinkIndex + lengthOfAsanaLink) {
+        return;
+      }
+
+      const taskId = getAsanaTaskId(line.text);
+
+      return new vscode.Hover(`The Asana Task ID is ${taskId}`);
     },
   };
 
